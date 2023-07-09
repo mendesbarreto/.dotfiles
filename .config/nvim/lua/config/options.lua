2b02547ff44f51a2af6918dca2ed482d9234e888
Douglas <mendes-barreto@Live.com>
2023-07-09 16:42:00 -0400

    chore: Add lazy steup

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,13 @@
+local opt = vim.opt
+
+opt.foldlevel = 20
+opt.foldmethod = "expr"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
+-- Save changes from vim to the system clipboard
+opt.clipboard = "unnamed"
+-- The scroll offse will be 15 lines
+opt.scrolloff = 15
+
+vim.o.cmdheight = 0
+vim.o.showcmdloc = "statusline"
+vim.o.winbar = "%{%v:lua.require'nvim-navic'.get_location()%}"
