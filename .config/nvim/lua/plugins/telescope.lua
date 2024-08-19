977c221685c24ab3322382e77ba9ec1371059e96
Douglas Mendes <mendes-barreto@live.com>
2024-08-18 21:03:00 -0400

    chore: Remove unnecessary variables

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -11,7 +11,6 @@ return {
     },
   },
   opts = function(_, opts)
-    local actions = require("telescope.actions")
     if not opts.defaults then
       opts.defaults = {}
     end
@@ -22,29 +21,16 @@ return {
       opts.defaults.mappings.n = {}
     end
 
-    local find_files_no_ignore = function()
+    local find_files_no_ignore = function(prompt_bufnr)
       local action_state = require("telescope.actions.state")
       local line = action_state.get_current_line()
+      local current_picker = action_state.get_current_picker(prompt_bufnr)
       if current_picker.prompt_title == "Live Grep" then
         LazyVim.pick("live_grep", { hidden = true, no_ignore = true, default_text = line })()
       else
         LazyVim.pick("find_files", { hidden = true, no_ignore = true, default_text = line })()
       end
     end
-
-    local find_files_src = function(prompt_bufnr)
-      local action_state = require("telescope.actions.state")
-      local line = action_state.get_current_line()
-      local cwd = vim.uv.cwd() .. "/src/"
-      local current_picker = action_state.get_current_picker(prompt_bufnr)
-      if current_picker.prompt_title == "Live Grep" then
-        LazyVim.pick("live_grep", { hidden = true, no_ignore = true, cwd = cwd, default_text = line })()
-      else
-        LazyVim.pick("find_files", { hidden = true, no_ignore = true, cwd = cwd, default_text = line })()
-      end
-    end
-
     opts.defaults.mappings.i["<C-h>"] = find_files_no_ignore
-    opts.defaults.mappings.i["<C-j>"] = find_files_src
   end,
 }
